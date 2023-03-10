f644f56b2c5e7d9a3acf0666c98d63f8648ef3f3
Jakub Stepnik <jakubstepni1k@gmail.com>
2023-03-10 17:36:11 +0100

    navbar

---
+++ main.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+const primaryHeader = document.querySelector(".primary-header");
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("arial-expanded", false)
+    : navToggle.setAttribute("arial-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+});
