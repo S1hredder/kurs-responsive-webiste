6aa3cfb41e18ac3d3e79ab6c0d294f40ed9ef599
Jakub Stepnik <jakubstepni1k@gmail.com>
2023-03-16 17:22:09 +0100

    slider done

--- main.js
+++ main.js
@@ -36,61 +36,61 @@ navToggle.addEventListener("click", () => {
 //   });
 // });
 
+// const slider = tns({
+//   container: ".my-slider",
+//   preventScrollOnTouch: "auto",
+//   navPosition: "bottom",
+//   controls: false,
+//   autoplay: true,
+//   autoplayTimeout: 3000,
+//   autoplayButtonOutput: false,
+//   center: true,
+//   items: 1,
+//   mouseDrag: true,
+//   responsive: {
+//     900: {
+//       items: 3,
+//       speed: 200,
+//       gutter: 30,
+//       nav: false,
+//     },
+//     600: {
+//       items: 2,
+//       gutter: 30,
+//       nav: false,
+//     },
+//   },
+// });
 const slider = tns({
   container: ".my-slider",
-  preventScrollOnTouch: "auto",
-  navPosition: "bottom",
-  controls: false,
-  autoplay: true,
-  autoplayTimeout: 3000,
-  autoplayButtonOutput: false,
-  slideBy: "page",
-  center: true,
   items: 1,
   mouseDrag: true,
-  // responsive: {
-  //   900: {
-  //     items: 3,
-  //     speed: 200,
-  //     gutter: 30,
-  //     nav: false,
-  //   },
-  //   600: {
-  //     items: 2,
-  //     gutter: 30,
-  //     nav: false,
-  //   },
-  // },
+  slideBy: "page",
+  swipeAngle: false,
+  speed: 400,
+  gutter: 30,
+  controls: false,
+  autoplay: true,
+  navPosition: "bottom",
+  controls: false,
+  autoplayButtonOutput: false,
+  preventScrollOnTouch: "force",
+  responsive: {
+    900: {
+      items: 3,
+      speed: 200,
+      gutter: 30,
+      nav: false,
+    },
+    600: {
+      items: 2,
+      gutter: 30,
+      nav: false,
+    },
+  },
 });
-
 const tnsNav = document.querySelector(".tns-nav").childNodes;
 tnsNav[0].classList.add("button-special-slider");
-// automatic change button slier after time
-// let info = "";
-// let displayIndex = 0;
-
-// const changeButtonSlider = () => {
-//   // console.log(document.querySelector(".tns-slide-active"));
-//   info = slider.getInfo();
-//   displayIndex = info.displayIndex;
-//   console.log(displayIndex);
-//   document
-//     .querySelector(".button-special-slider")
-//     .classList.remove("button-special-slider");
-//   tnsNav[displayIndex - 1].classList.add("button-special-slider");
-// };
-
-// let changeButtonSliderAuto = setInterval(changeButtonSlider, 3050);
-// const sliderCard = document.querySelectorAll(".slider-card");
-// sliderCard.forEach((element) => {
-//   element.addEventListener("mousedown", () => {
-//     console.log("test");
-//     clearInterval(changeButtonSliderAuto);
-//     element.addEventListener("mouseup", () => {
-//       changeButtonSliderAuto = setInterval(changeButtonSlider, 3050);
-//     });
-//   });
-// });
 //change on click button slider
 tnsNav.forEach((element) => {
   element.addEventListener("click", () => {
@@ -102,10 +102,8 @@ tnsNav.forEach((element) => {
   });
 });
 
-let count = 0;
-const customizedFunction = (info, eventName) => {
-  count++;
-  console.log(info.displayIndex);
+const changeButtonColor = (info) => {
+  // console.log(info.displayIndex);
   const displayIndex = info.displayIndex;
   document
     .querySelector(".button-special-slider")
@@ -113,4 +111,4 @@ const customizedFunction = (info, eventName) => {
   tnsNav[displayIndex - 1].classList.add("button-special-slider");
 };
 
-slider.events.on("indexChanged", customizedFunction);
+slider.events.on("indexChanged", changeButtonColor);
